edd64c840a8cd07ecb70e9618a6b0c5ed1339e78
voidxog <ahmedarefinsajjad@gmail.com>
2025-12-19 20:11:32 +0600

    lavender: Drop freeform multiwindow

--- device.mk
+++ device.mk
@@ -177,10 +177,6 @@ PRODUCT_PACKAGES += \
     qcom.fmradio \
     qcom.fmradio.xml
 
-# Freeform Multiwindow
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
-
 # Fstab
 PRODUCT_PACKAGES += \
     fstab.qcom
